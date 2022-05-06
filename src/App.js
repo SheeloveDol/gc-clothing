424fa4a82e496563985cfdc9f9ce15fbdc82e623
Sheelove Dol <siesel26@gmail.com>
2022-05-06 13:56:28 -0700

    Testing Router, Route, and Outlet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component'
 
 
 const App = () => {
 
+  const Navigation = () => {
+    return (
+      <div>
+        <div>
+          <h1>I am a Nav Bar</h1>
+        </div>
+        <Outlet />
+      </div>
+    )
+  }
+
+  const Apple = () => {
+    return (
+      <h1>This is the Apple store page. Still under construction</h1>
+    )
+  }
+
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />} >
+      <Route index element={<Home />} />
+      <Route path='/apple' element={<Apple />} />
+      </Route>
+      
     </Routes>
   )
 }
